e5d9776463158c6b323f6cb0edd1366715091654
HEMANTAMRUTE <hemantamrute12345@gmail.com>
2025-06-23 20:32:48 +0530

    Set up socket.io connection and environment config for deployment

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const {Server}=require("socket.io");
-const PORT = process.env.PORT || 8000;
-const FRONT=process.env.FRONT;
-const io=new Server(PORT,
-    {
-        cors:FRONT,
-    }
-);
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Change this to your frontend URL in production
+    methods: ["GET", "POST"]
+  }
+});
+
+// Socket event handlers (same as your code)
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
-io.on("connection",(socket)=>{
-    console.log(`socketconnected`,socket.id);
-    socket.on("room:join", (data) => {
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
+
+  socket.on("room:join", (data) => {
     const { email, room } = data;
     console.log(data);
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
     io.to(room).emit("user:joined", { email, id: socket.id });
     socket.join(room);
-     io.to(socket.id).emit("room:join", data);
-    });
+    io.to(socket.id).emit("room:join", data);
+  });
 
-     socket.on("user:call", ({ to, offer }) => {
+  socket.on("user:call", ({ to, offer }) => {
     io.to(to).emit("incomming:call", { from: socket.id, offer });
   });
-    
-    socket.on("call:accepted", ({ to, ans }) => {
+
+  socket.on("call:accepted", ({ to, ans }) => {
     io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
 
-   socket.on("peer:nego:needed", ({ to, offer }) => {
+  socket.on("peer:nego:needed", ({ to, offer }) => {
     console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
   });
@@ -38,7 +46,10 @@ io.on("connection",(socket)=>{
     console.log("peer:nego:done", ans);
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
+});
 
-})
-
-
+// Listen on a dynamic port for deployment or default to 8000 locally
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
